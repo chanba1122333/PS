3e735d483c7a04b0c01ca950789bc1d035c3ef46
chanha lee <chking1231234@gmail.com>
2025-10-23 19:12:06 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef pair<ull, ull> pull;
+typedef const ll cll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+typedef priority_queue<ll> pqll;
+typedef priority_queue<pll> pqpll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<vpll> vvpll;
+#define FOR(i, a, A) for (ll i = a; i < A; ++i)
+#define IFOR(i, a, A) for (ll i = a; i >= A; --i)
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << 56*a + 24*b + 14*c + 6*d << '\n';
+    return 0;
+}
+ 
