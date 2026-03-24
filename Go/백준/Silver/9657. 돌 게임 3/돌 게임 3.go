a1634038070574b69e263a4ee56ebd770fdc7f44
chanha lee <chking1231234@gmail.com>
2026-03-24 19:28:41 +0900

    [Silver III] Title: 돌 게임 3, Time: 4 ms, Memory: 996 KB -BaekjoonHub

---
+++ Go/백준/Silver/9657. 돌 게임 3/돌 게임 3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscanln(reader, &n)
+	if n%7 == 0 || (n-2)%7 ==0 {
+		fmt.Fprintln(writer, "CY")
+	} else {
+		fmt.Fprintln(writer, "SK")
+	}
+}
+
