ccd04bd385fe6e12c67ab5bab9462ec223fee748
chanha lee <chking1231234@gmail.com>
2026-03-25 14:32:47 +0900

    [Bronze V] Title: N 찍기, Time: 16 ms, Memory: 1768 KB -BaekjoonHub

---
+++ Go/백준/Bronze/2741. N 찍기/N 찍기.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	for i:= 1; i<=n; i++ {
+		fmt.Fprintln(writer,i)
+	}
+}
