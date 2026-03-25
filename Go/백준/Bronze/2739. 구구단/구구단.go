a2d8c5aa91c4b00703df31f7d1ca65a71824e3f2
chanha lee <chking1231234@gmail.com>
2026-03-25 14:55:04 +0900

    [Bronze V] Title: 구구단, Time: 4 ms, Memory: 964 KB -BaekjoonHub

---
+++ Go/백준/Bronze/2739. 구구단/구구단.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	for i:=1; i<=9; i++ {
+	fmt.Fprintln(writer, n , "*", i, "=", n*i)
+	}
+	
+}
