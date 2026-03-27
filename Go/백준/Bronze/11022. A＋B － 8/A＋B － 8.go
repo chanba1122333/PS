1aaf00c6117255474db0e59c6c34e232240ecb4d
chanha lee <chking1231234@gmail.com>
2026-03-27 18:13:43 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 984 KB -BaekjoonHub

---
+++ Go/백준/Bronze/11022. A＋B － 8/A＋B － 8.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    writer := bufio.NewWriter(os.Stdout)
+    defer writer.Flush()
+
+    var t, a, b int
+    fmt.Fscan(reader, &t)
+
+    for i := 1; i <= t; i++ { 
+        fmt.Fscan(reader, &a, &b)
+        fmt.Fprintf(writer, "Case #%d: %d + %d = %d\n", i, a, b, a+b)
+    }
+}
