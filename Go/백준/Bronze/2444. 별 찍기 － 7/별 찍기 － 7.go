75acc1bd113ff8dbe6fde4441f54f77cf8365463
chanha lee <chking1231234@gmail.com>
2026-03-30 00:00:25 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 656 ms, Memory: 964 KB -BaekjoonHub

---
+++ Go/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    for i := 1; i <= n; i++ {
+
+        for j := 0; j < n-i; j++ {
+            fmt.Print(" ")
+        }
+
+        for j := 0; j < 2*i-1; j++ {
+            fmt.Print("*")
+        }
+        fmt.Println() 
+    }
+
+
+    for i := 1; i < n; i++ {
+
+        for j := 0; j < i; j++ {
+            fmt.Print(" ")
+        }
+
+        for j := 0; j < 2*(n-i)-1; j++ {
+            fmt.Print("*")
+        }
+        fmt.Println()
+    }
+}
