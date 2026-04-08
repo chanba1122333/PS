27fb00e0bd4dc29e7fa6339c82cc879d0bd7e168
chanha lee <chking1231234@gmail.com>
2026-04-08 17:35:52 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 4 ms, Memory: 980 KB -BaekjoonHub

---
+++ Go/백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+	n = n/11 * 10
+	fmt.Fprintln(writer, n)
+	
+}
